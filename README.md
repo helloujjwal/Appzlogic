387994f3adbbd1226316fe15f034e704508f4767
Ujjwal Singh <ujjwal@Ujjwals-MacBook-Pro.local>
2024-10-01 16:18:56 +0530

    creadted pom file test file and updated readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# Appzlogic
+
+Description:-
+
+Booking a Flight on Clear Trip Web Application Testing by Using:
+
+Playwright https://playwright.dev/
+TypeScript https://www.typescriptlang.org/
+This tests are purely for Playwright features practice, usage of Page Object Model.
+
+Getting Started
+
+Prerequisites
+
+Install Playwright and browser binaries for Chromium, Firefox and WebKit.
+
+```bash
+npm init playwright@latest
+```
+
+You need to have Node.js installed.
+
+Useful Commands
+
+Open terminal in the project root directory
+
+Run all Tests
+
+```bash
+npx playwright test
+```
+
+Run Tests for clear Trip Feature
+
+```bash
+npx playwright test clearTrip/tests/clearTrip.spec.ts
+```
+
+Run Tests in headed mode for clear Trip Feature
+
+```bash
+npx playwright test clearTrip/tests/clearTrip.spec.ts --headed
+```
--- clearTrip/src/cleartrip.locator.ts
+++ clearTrip/src/cleartrip.locator.ts
@@ -6,7 +6,7 @@ const searchFlights = {
     where_to: "[class='dropdown-container h-13 bc-line-500 flex ba-solid mt-2  '] [class='field-2 flex h-13 flex-middle p-relative pr-4 w-100p ']",
     select_city_where_to: "[class='dropdown-container h-13 bc-line-500 flex ba-solid mt-2  '] [class='field-2 flex h-13 flex-middle p-relative pr-4 w-100p '] [class='airportList']",
     calender: "[class='flex flex-middle p-relative homeCalender'] [class='sc-aXZVg dSvAMK mr-2 mt-1']",
-    booking_date: "[class='flex flex-middle p-relative homeCalender'] [class='DayPicker-Body'] [aria-label='Tue Oct 01 2024']",
+    booking_date: "[class='flex flex-middle p-relative homeCalender'] [class='DayPicker-Body'] [aria-label='Fri Oct 04 2024']",
     return_field: "[class*='focus:bc-secondary-500 t-all'] [class=' c-neutral-400 flex flex-nowrap fs-16 fw-500']",
     retrun_date: "[class='flex flex-middle p-relative homeCalender'] [aria-label='Sat Oct 12 2024']",
     search_flight_button: "[class='sc-dAlyuH cIApyz']"
